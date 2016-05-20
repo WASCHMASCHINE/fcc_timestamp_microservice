f4b75d2094e61ff7097c1335ebe205c81b121d2b
Waschmaschine <jonas.haeling@gmail.com>
2016-05-20 19:27:59 +0000

    cleaned code of clementine stuff

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
 function getMonth(index){
 	switch (index) {
@@ -61,17 +48,6 @@ app.get('/:time', function(req, res){
 		res.send({"unix":null, "natural": null});		
 	}
 });
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
